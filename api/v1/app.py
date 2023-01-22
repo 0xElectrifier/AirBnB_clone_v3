6007675d1e98cff31a4dc692481e87555b8eb380
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-23 00:17:46 +0100

    Create error handler for status-404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import Blueprint
 from flask import Flask
+from flask import jsonify
 from models import storage
 from os import getenv
 
@@ -23,6 +24,12 @@ app.register_blueprint(app_views)
 def teardown(exc):
     """An operation to run after every application context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Returns a view on error 404"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
