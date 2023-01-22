2b7478aec237b44605ed88576dbc53c6b207f99b
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-22 20:36:02 +0100

    Resolved bug, from: request.get_json method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,31 +2,33 @@
 """Defines the Amenity api"""
 from api.v1.views import app_views
 from flask import abort
+from flask import jsonify
 from flask import request
 from models import storage
 
 
-@app_views.route('/amenities', methods=["GET"])
-@app_views.route('/amenities/<amenity_id>', methods=["GET"])
+@app_views.route('/amenities', strict_slashes=False, methods=["GET"])
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False,
+                 methods=["GET"])
 def fetch_amenity(amenity_id=None):
     """Retrieves a list of Amenity objects if @amenity_id is specified
     or just one object if argument is not none
     """
-    if amenity_id == None:
+    if amenity_id is None:
         obj = storage.all("Amenity")
         for key, value in obj.items():
             obj[key] = value.to_dict()
     else:
         obj = storage.get("Amenity", amenity_id)
-        if obj == None:
+        if obj is None:
             abort(404)
         obj = obj.to_dict()
-
 
     return obj
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False,
+                 methods=['DELETE'])
 def delete_amenity(amenity_id):
     """Deletes an Amenity object from database"""
     key = "Amenity." + amenity_id
@@ -37,36 +39,48 @@ def delete_amenity(amenity_id):
         abort(404)
 
 
-@app_views.route('/amenities', methods=["POST"])
+@app_views.route('/amenities', strict_slashes=False,
+                 methods=["POST"])
 def post_amenity():
     """Handles post request to the `Amenity` class"""
-    data = request.get_json()
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
     if not data:
-        abort(400, description="Not a JSON")
+            return "Not a JSON", 400
 
     try:
         name = data['name']
     except KeyError:
-        abort(400, description='Missing name')
+        return 'Missing name', 400
 
     from models.amenity import Amenity
     new_obj = Amenity(**data)
     new_obj.save()
 
+    return jsonify(new_obj.to_dict()), 201
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False,
+                 methods=['PUT'])
 def update_amenity(amenity_id):
     """Makes an HTTP PUT request, to update the Amenity objects in database"""
     obj = storage.get("Amenity", amenity_id)
     if obj is None:
         abort(404)
 
-    data = request.get_json()
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
     if not data:
-        abort(400, description="Not a JSON")
+            return "Not a JSON", 400
+
     for key, value in data.items():
         if key in ['id', 'created_at', 'updated_at']:
             continue
         setattr(obj, key, value)
 
-    return obj, 200
+    obj.save()
+    return jsonify(obj.to_dict()), 200
