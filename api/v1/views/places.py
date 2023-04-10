aea885bec6a212cba3617db1c53412da90896733
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-04-10 18:52:21 +0100

    Updating places_search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -90,6 +90,7 @@ def update_places(place_id):
     return jsonify(place.to_dict()), 200
 
 
+"""
 @app_views.route('/places_search', strict_slashes=False,
                  methods=["POST"])
 def search_places():
@@ -102,11 +103,14 @@ def search_places():
 
     classes = ["amenities", "cities", "states"]
     result_places = []
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
+    if HBNB_API_HOST is None:
+        HBNB_API_HOST = '0.0.0.0'
     if HBNB_API_PORT is None:
         HBNB_API_PORT = '5050'
 
-    root_url = 'http://0.0.0.0:{}/api/v1'.format(HBNB_API_PORT)
+    root_url = 'http://{}:{}/api/v1'.format(HBNB_API_HOST, HBNB_API_PORT)
 
     for key, id_list in data.items():
         if key not in classes:
@@ -123,7 +127,9 @@ def search_places():
                     c_places = requests.get(c_places_url.format(sc_id))
                     if c_places.status_code > 299:
                         continue
-                    result_places.append(place for place in c_places)
+                    for place_1 in c_places:
+                        result_places.append(place_1)
+                    # result_places.append(place for place in c_places)
         elif key == "cities":
             for city_id in id_list:
                 curr_city_url = root_url + '/cities/{}'.format(city_id)
@@ -139,7 +145,8 @@ def search_places():
                 c_places_url = root_url + '/cities/{}/places'
                 c_places_2 = requests.get(c_places_url.format(city_id))
                 c_places_2 = c_places_2.json()
-                result_places.append(place for place in c_places_2)
+                for place in c_places_2:
+                    result_places.append(place)
 
     # If at this point 'result_places' is empty, fetch all 'Place' objects
     if len(result_places) == 0:
@@ -159,3 +166,66 @@ def search_places():
                     result_places.remove(place)
 
     return jsonify(result_places), 200
+"""
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/place/post_search.yml', methods=['POST'])
+def places_search():
+    """
+    Retrieves all Place objects depending of the JSON in the body
+    of the request
+    """
+
+    if request.get_json() is None:
+        abort(400, description="Not a JSON")
+
+    data = request.get_json()
+
+    if data and len(data):
+        states = data.get('states', None)
+        cities = data.get('cities', None)
+        amenities = data.get('amenities', None)
+
+    if not data or not len(data) or (
+            not states and
+            not cities and
+            not amenities):
+        places = storage.all(Place).values()
+        list_places = []
+        for place in places:
+            list_places.append(place.to_dict()) 
+        return jsonify(list_places) 
+ 
+    list_places = [] 
+    if states: 
+        states_obj = [storage.get(State, s_id) for s_id in states] 
+        for state in states_obj: 
+            if state: 
+                for city in state.cities: 
+                    if city: 
+                        for place in city.places: 
+                            list_places.append(place) 
+ 
+    if cities: 
+        city_obj = [storage.get(City, c_id) for c_id in cities]
+        for city in city_obj:
+            if city:
+                for place in city.places:
+                    if place not in list_places:
+                        list_places.append(place)
+
+    if amenities:
+        if not list_places:
+            list_places = storage.all(Place).values()
+        amenities_obj = [storage.get(Amenity, a_id) for a_id in amenities]
+        list_places = [place for place in list_places
+                       if all([am in place.amenities
+                               for am in amenities_obj])]
+
+    places = []
+    for p in list_places:
+        d = p.to_dict()
+        d.pop('amenities', None)
+        places.append(d)
+
+    return jsonify(places)
